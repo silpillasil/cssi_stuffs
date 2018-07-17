ff8729c2ea084d07ecd10399ad78c0e6cd1cdc5a
Demo User <demouser@MacBook-Pro.local>
2018-07-17 16:01:34 -0500

    bloorp

---
+++ javascriptt/javascript.js
@@ -0,0 +1,12 @@
+console.log('Hello World!');
+const name = 'Charlie';
+console.log(`Hello ${name}!`);
+const age = 16;
+console.log('You are '+age+' years old!');
+if (age>=15)
+{
+  console.log("You can get your driver's license.");
+} else
+{
+  console.log("You will have to wait until you're 15.");
+}
